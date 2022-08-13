641c0e77de85c074a07ee46f0de6419b6bfff294
KemiDoyin <kemidoy@gmail.com>
2022-08-13 23:11:33 +0100

    add eventlistener for dice

---
+++ advice-generator-app-main/index.js
@@ -0,0 +1,20 @@
+const slipId = document.getElementById('slip__id');
+const adviceSlip = document.getElementById('advice__slip');
+const dice = document.querySelector('.dice');
+const APIURL = 'https://api.adviceslip.com/advice'
+
+
+
+dice.addEventListener('click', async () => {
+    try {
+        const res = await axios.get(APIURL)
+        adviceSlip.textContent =
+        res.data.slip.advice
+        slipId.textContent = res.data.slip.id
+        console.log(res.data)
+        console.log(res.data.slip)
+    }
+    catch(err) {
+        console.log(err)
+    }
+})
